671cab0a9b09279e86da9caa2c4e558fbb325508
Rao <SRao7@ad.harman.com>
2020-12-13 23:20:42 +0530

    First Commit to the cool apps

---
+++ index.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+
+app.set('view-engine','ejs'); //Setting the view engine to show the template.
+app.use(express.static(__dirname + '/assets'));
+
+app.get("/",(req,res)=>{
+    res.send('Hello World');
+});
+
+app.get("/login",(req,res)=>{
+     res.render("login.ejs"); 
+})
+
+app.listen(5000);
