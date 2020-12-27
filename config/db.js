2c658e03ee13374b847c5762b2171d90aa81b746
Rao <SRao7@ad.harman.com>
2020-12-27 20:49:38 +0530

    Custom MySQL setup

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 var db={
-     database: "coolapps",
-     hostname:"localhost",
-     username:"root",
-     password:""
+     database:"sql12384032",
+     hostname:"sql12.freemysqlhosting.net",
+     username:"sql12384032",
+     password:"HRCZVjPdYN"
 }
 
 module.exports=db;
